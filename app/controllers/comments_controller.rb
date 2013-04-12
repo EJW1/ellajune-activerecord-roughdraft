41186f15c55b94969ef1c8849f5f0905250e4fc6
Ericka Ward <ericka.ward@gmail.com>
2013-04-11 20:44:57 -0700

    show all comments for the link in that links show view with the comments form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -42,17 +42,8 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
-    @comment = current_user.comments.new(params[:comment])
-
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
+    @comment = current_user.comments.create(params[:comment])
+    redirect_to :back
   end
 
   # PUT /comments/1
