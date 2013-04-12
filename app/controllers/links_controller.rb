06666ebb1c3251caacb48279784ce28471a767db
Ericka Ward <ericka.ward@gmail.com>
2013-04-11 19:40:17 -0700

    comments form and view

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,6 +15,7 @@ class LinksController < ApplicationController
   # GET /links/1.json
   def show
     @link = Link.find(params[:id])
+    @comment = Comment.new
 
     respond_to do |format|
       format.html # show.html.erb
