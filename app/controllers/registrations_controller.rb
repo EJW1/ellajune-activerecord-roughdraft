6064362701c6a608052abf03b7ed7801a651773a
Ericka Ward <ericka.ward@gmail.com>
2013-05-01 14:52:52 -0700

    fixing user index page

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,7 +7,7 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.new(params[:user])
     user_profile = @user.build_user_profile
-    if @user.save
+    if (@user.save && @user.user_profile.save)
       flash[:notice] = "You have signed up successfully."
       redirect_to root_url
     else
