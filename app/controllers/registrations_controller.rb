e20d0a4f1c8cc8655505a82cd6b41dfba7598d15
Ericka Ward <ericka.ward@gmail.com>
2013-05-01 16:31:41 -0700

    woohoo! finally, I figured out why the user profile was not saving

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,10 @@ class RegistrationsController < Devise::RegistrationsController
     def new
       super
     end
-
   
   def create
     @user = User.new(params[:user])
-    user_profile = @user.build_user_profile
+    #user_profile = @user.build_user_profile
     if (@user.save && @user.user_profile.save)
       flash[:notice] = "You have signed up successfully."
       redirect_to root_url
