bfc19d55dfcd04dd1a4e4817bf410a5daa5a1789
Ericka Ward <ericka.ward@gmail.com>
2013-05-03 11:18:39 -0700

    initial commit for messaging system

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502053118) do
+ActiveRecord::Schema.define(:version => 20130503181806) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(:version => 20130502053118) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "title"
+  end
+
+  create_table "messages", :force => true do |t|
+    t.string   "subject"
+    t.text     "body"
+    t.datetime "read_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "user_profiles", :force => true do |t|
