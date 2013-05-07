7afe8279b7dee95743b2c7adb9e51c4b394438db
Ericka Ward <ericka.ward@gmail.com>
2013-05-07 11:18:26 -0700

    create link_tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130503182321) do
+ActiveRecord::Schema.define(:version => 20130507181731) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -32,6 +32,12 @@ ActiveRecord::Schema.define(:version => 20130503182321) do
   add_index "interests_taggings", ["user_profile_id"], :name => "index_interests_taggings_on_user_profile_id"
 
   create_table "interests_tags", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "link_tags", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
