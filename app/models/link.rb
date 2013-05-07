cc0d3f4a61a5701dd430f9234d05c54864dd2760
Ericka Ward <ericka.ward@gmail.com>
2013-05-07 11:29:23 -0700

    link model has link_tag associations and methods

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,27 @@
 class Link < ActiveRecord::Base
-  attr_accessible :url, :user_id, :title
+  attr_accessible :url, :user_id, :title, :link_tag_list
   belongs_to :user
   has_many :comments
   has_many :votes
+  has_many :link_taggings
+  has_many :link_tags, through: :link_taggings
+
+  def self.tagged_with(name)
+    LinkTag.find_by_name!(name).links
+  end
+
+  def self.tag_counts
+    LinkTag.select("link_tags.*, count(link_taggings.link_tag_id) as count").
+      joins(:link_taggings).group("link_taggings.link_tag_id")
+  end
+  
+  def tag_list
+    link_tags.map(&:name).join(", ")
+  end
+  
+  def tag_list=(names)
+    self.link_tags = names.split(",").map do |n|
+      LinkTag.where(name: n.strip).first_or_create!
+    end
+  end
 end
