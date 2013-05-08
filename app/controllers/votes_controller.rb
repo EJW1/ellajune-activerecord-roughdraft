9f29caba943a1d436a992fb6ec2ba0849dfaee7f
Ericka Ward <ericka.ward@gmail.com>
2013-05-08 15:27:23 -0700

    vote create view debugged

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -42,6 +42,15 @@ class VotesController < ApplicationController
   def create
     if current_user
       @vote = current_user.votes.create(params[:vote])
+      respond_to do |format|
+        if @vote.save
+         format.html { redirect_to :back, notice: 'Link was successfully created.' }
+         format.json { render json: @vote, status: :created, location: @vote }
+        else
+          format.html { render action: "new" }
+          format.json { render json: @link.errors, status: :unprocessable_entity }
+        end
+      end
     else
       redirect_to :back, alert: "You must sign in to vote"
     end
