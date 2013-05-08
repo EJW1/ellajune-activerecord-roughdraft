046e3d08b61c01310ea55ef49ed90d9226c60a55
Ericka Ward <ericka.ward@gmail.com>
2013-05-08 14:30:09 -0700

    add points column to links table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130507193551) do
+ActiveRecord::Schema.define(:version => 20130508212846) do
 
   create_table "comments", :force => true do |t|
     t.text     "message"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(:version => 20130507193551) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.string   "title"
+    t.integer  "points"
   end
 
   create_table "messages", :force => true do |t|
